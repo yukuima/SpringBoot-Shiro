37f9a4ce2918567ccc75525dd2cfef65598d4294
yukuima <58396683@qq.com>
2019-06-05 21:46:52 +0800

    commit

---
+++ src/main/java/com/myk/learning/shiro/dao/SysRoleDao.java
@@ -0,0 +1,8 @@
+package com.myk.learning.shiro.dao;
+
+import com.myk.learning.shiro.model.SysRole;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SysRoleDao extends CrudRepository<SysRole, Integer> {
+
+}
